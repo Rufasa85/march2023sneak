7ed0bce1d7692b1e2856409935e7f8f26f82a2ea
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-04-24 11:46:43 -0700

    adding paramets

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 const express = require("express");
 const app = express();
+const sneakers = [
+    {
+        id:1,
+        owner:"Joe",
+        name:"Converse All Stars",
+        color:"red"
+    },
+    {   
+        id:2,
+        owner:"Joe",
+        name:"Air Jordans",
+        color:"white"
+    },
+    {
+        id:3,
+        owner:"Tom",
+        name:"Birkenstocks",
+        color:"brown"
+    },
+    {
+        id:4,
+        owner:"Shiva",
+        name:"CatPaw",
+        color:"orange"
+    }
+]
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the home page")
 })
 
 app.get('/sneakers', (req,res)=>{
-    res.json([
-        {
-            name:"Converse All Stars",
-            color:"Red"
-        },
-        {
-            name:"Air Jordans",
-            color:"White"
-        },
-    ])
+    res.json(sneakers)
 })
+
+app.get("/sneakers/:sneakId",(req,res)=>{
+    const sneakId = req.params.sneakId;
+    for (let i = 0; i < sneakers.length; i++) {
+        if(sneakers[i].id==sneakId){
+            return res.json(sneakers[i])
+        }   
+    }
+    return res.status(404).json({
+        msg:"no such sneaker!"
+    })
+})
+
+// app.get("/sneakers/1",(req,res)=>{
+//     res.json(sneakers[0])
+// })
+
+// app.get("/sneakers/2",(req,res)=>{
+//     res.json(sneakers[1])
+// })
+
+// app.get("/sneakers/3",(req,res)=>{
+//     res.json(sneakers[2])
+// })
 
 app.listen(3000,()=>{
     console.log("listening on port 3000!")
